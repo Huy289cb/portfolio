1f94b77d9fb87ecf9fa48e2095f5b19e39cbe67f
huylq289cb <luonghuyn2cb@gmail.com>
2023-02-23 23:38:57 +0700

    add animate works, bio section

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -7,7 +7,7 @@ const Main = ({ children, router }) => {
     return <Box as="main" pb={8}>
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-            <title>huy289cb - Homepage</title>
+            <title>Huy289cb - Homepage</title>
         </Head>
         <Navbar path={router.asPath} />
         <Container maxW="container.md" pt={14}>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
-import { Container, Box, Heading, Image } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+  Link,
+  Button
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Paragraph from '../components/paragraph'
+import Section from '../components/section'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="green.500" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I&apos;m a front-end developer base in VietNam! 💙
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -11,7 +30,7 @@ const Page = () => {
           <Heading as="h2" variant="page-title">
             Lương Quốc Huy
           </Heading>
-          <p>Digital Craftman ( Developer / Designer / Gamer )</p>
+          <p>Digital Craftman ( Developer / Designer / Wjbu )</p>
         </Box>
         <Box
           flexShrink={0}
@@ -25,12 +44,55 @@ const Page = () => {
             borderStyle="solid"
             maxWidth="100px"
             display="inline-block"
-            borderRadius="8px"
+            borderRadius="lg"
             src="images/huy.jpg"
             alt="Profile Image"
           />
         </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          Huy is a freelance and a full-stack developer based in Ha Noi with a
+          passion for building digital services/stuff he wants. He has a knack
+          for all things launching products, from planning and designing all the
+          way to solving real-life problems with code. When not online, he loves
+          hanging out with his camera. Currently he is living off of his own
+          product called{' '}
+          <Link as={NextLink} href="/works/inkdrop">Inkdrop</Link>
+          .
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button
+              rightIcon={<ChevronRightIcon />}
+              colorScheme="teal"
+            >
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1998</BioYear>
+          Born in Cao Bang (Cao Bằng), Viet Nam.
+        </BioSection>
+        <BioSection>
+          <BioYear>2022</BioYear>
+          Completed the Engineer&apos;s Program in the Software Technology
+          at Ha Noi Open University.
+        </BioSection>
+        <BioSection>
+          <BioYear>2022 to present</BioYear>
+          Works as a developer at Globits Co., Ltd.
+        </BioSection>
+      </Section>
     </Container>
   )
 }
